0d487931b76e6efde32d852b9b4c77510f8af257
somyak simant <53150525+somnov18@users.noreply.github.com>
2020-03-20 20:11:36 +0530

    Basic Arithmetic operations
    Java program to perform basic arithmetic operations such as addition, subtraction, multiplication and division.

---
+++ arithmetic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class arithmetic{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter first number:");
+        int a = sc.nextInt();
+        System.out.println("enter second number:");
+        int b= sc.nextInt();
+        System.out.println("a+b="+ (a+b));
+        System.out.println("a-b="+ (a-b));
+        System.out.println("a*b="+ (a*b));
+        System.out.println("a/b="+ (a/b));
+    }
+}
