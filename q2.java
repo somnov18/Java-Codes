43c41e7215976ebe18a4b580666aca8989496f45
somyak simant <53150525+somnov18@users.noreply.github.com>
2020-03-27 16:09:27 +0530

    Inheritance
    Java program to show single level inheritance.

--- q2.java
+++ q2.java
@@ -1,50 +1,15 @@
-import java.util.Scanner;
+class base{
+    void show(){
+        System.out.print("parent method");
+    }
+}
+class q2 extends base{
+    void show(){
+        System.out.print("child method");
+    }
+    public static void main(String[] args) {
 
-public class q2
-{
-    
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        int a[][]=new int[3][3];
-        int sum1=0,sum2=0;
-        System.out.println("Enter 9 Numbers");
-        //input
-        for(int i=0;i<3;i++)
-        {
-            for(int j=0;j<3;j++)
-            {
-                a[i][j]=sc.nextInt();
-            }
-        }
-        //left diagonal
-        for(int i=0;i<3;i++)
-        {
-            for(int j=0;j<3;j++)
-            {
-                if(i==j)
-                sum1=sum1+a[i][j];
-            }
-        }
-        //right Diagonal
-        for(int i=0;i<3;i++)
-        {
-            for(int j=0;j<3;j++)
-            {
-                if(i+j==2)
-                sum2=sum2+a[i][j];
-            }
-        }
-        //print
-        for(int i=0;i<3;i++)
-        {
-            for(int j=0;j<3;j++)
-            {
-                System.out.print(a[i][j]+" ");
-            }
-            System.out.println("");
-        }
-        System.out.println("sum of left diagonal="+ sum1);
-        System.out.println("sum of right diagonal="+ sum2);
+        base obj = new q2();
+        obj.show();
     }
 }
