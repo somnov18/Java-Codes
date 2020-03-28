65c51fb688770032fa61bd5fa15e56d27de5fca8
somyak simant <53150525+somnov18@users.noreply.github.com>
2020-03-28 15:53:23 +0530

    Constructor call in Inheritance
    Java program to show the sequence of calling of constructors, after inheritance.

--- q3.java
+++ q3.java
@@ -1,27 +1,15 @@
-import java.util.Scanner;
+class base{
+    public base(int x, int y){
+        System.out.print("parent parametrized constructor\n");
 
-public class q3
-{
-    
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter no. of elements:");
-        int i,j,n = sc.nextInt();
-        int a[]=new int[n];
-        System.out.println("Enter "+n+" Numbers");
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
-        }
-        System.out.println("enter the sum of the pair:");
-        int sum= sc.nextInt();
-        for(i=0;i<n;i++)
-        {
-            for(j=i+1;j<n;j++){
-                if(a[i]+a[j]==sum)
-                    System.out.println("("+a[i]+","+a[j]+")");
-            }
-        }
+    }
+}
+class q3 extends base{
+    public q3(int a, int b){
+        super(a,b);
+        System.out.print("child parametrized constructor");
+    }
+    public static void main(String[] args) {
+        q3 ob = new q3(1,2);
     }
 }
