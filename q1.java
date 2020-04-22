db24b375d83c60baa95a7b9b74fb34018ba51340
somyak simant <53150525+somnov18@users.noreply.github.com>
2020-04-22 19:50:19 +0530

    ArrayIndexOutOfBoundsException
    A Java program to generate an ArrayIndexOutofBoundsException and handle it using catch statement.

--- q1.java
+++ q1.java
@@ -1,32 +1,20 @@
-import java.util.Scanner;
 
-public class q1
-{
-    
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        int a[]=new int[10];
-        System.out.println("Enter 10 Numbers");
-        for(int i=0;i<10;i++)
-        {
-            a[i]=sc.nextInt();
+public class q1 {
+    public static void main(String [] args) {
+        int [] a = {1,2};
+        try{
+            a[4] = 5;
         }
-        for(int i=0; i<10; i++)
-        {
-            for(int j=i+1;j<10;j++)
-            {
-                if (a[i]>a[j])
-                {
-                    int temp=a[i];
-                    a[i]=a[j];
-                    a[j]=temp;
-                }
-            }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Array index out of bound");
         }
-        for(int i=0;i<10;i++)
-        {
-            System.out.print(a[i]+" ");
+        catch( Exception e){
+            System.out.println("Array index out of bound");
+        }
+        finally{
+            System.out.println("Finally block executed");
         }
     }
+    
+
 }
